1a08890cc9ff32310dc44c28eac3cb9a3ce6dae0
Austin Crim <austin@monogram.io>
2023-02-15 10:43:30 -0600

    use expressi integration for astro

--- src/astro/index.ts
+++ src/astro/index.ts
@@ -1,11 +1,1 @@
-import { pathToFileURL } from 'url'
-import type { Request } from 'firebase-functions/v2/https'
-import type { Response } from 'express'
-
-// assumes @astrojs/node adapter with mode: "middleware" config
-const serverEntry = import(`${pathToFileURL(process.cwd())}/entry.mjs`)
-
-export const handle = async (req: Request, res: Response) => {
-  const { handler } = await serverEntry
-  handler(req, res)
-}
+export * from '../express'
